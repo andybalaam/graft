c9cfff71318131ba1afb96d284a2fdf850c50723
Andy Balaam <andybalaam@artificialworlds.net>
2018-04-14 20:20:30 +0100

    Add a TODO to return Number from :R

--- graftlib/eval_.py
+++ graftlib/eval_.py
@@ -176,6 +176,7 @@ class Functions:
 
     def random(self):
         return float(self.rand.__call__(-10, 10))
+        # TODO: return Number
 
     def fork(self):
         self.fork_callback.__call__()
