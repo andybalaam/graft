054e8d53ceae24b952e66acbee722289919ca994
Andy Balaam <andybalaam@artificialworlds.net>
2018-05-15 17:25:35 +0100

    Import Cell's parser

---
+++ graftlib/parse2.py
@@ -0,0 +1,104 @@
+from graftlib.peekablestream import PeekableStream
+from graftlib.lex2 import (
+    AssignmentToken,
+    EndFunctionDefToken,
+    EndParamListToken,
+    ListSeparatorToken,
+    NumberToken,
+    OperatorToken,
+    ParamListPreludeToken,
+    StartFunctionDefToken,
+    StartParamListToken,
+    StatementSeparatorToken,
+    StringToken,
+    SymbolToken,
+)
+
+
+class Parser:
+    def __init__(self, tokens, stop_at):
+        self.tokens = tokens
+        self.stop_at = stop_at
+
+    def next_expression(self, prev):
+        self.fail_if_at_end(";")
+        tok = self.tokens.next
+        typ = type(tok)
+        if typ in self.stop_at:
+            return prev
+        self.tokens.move_next()
+        if (
+            typ in (NumberToken, StringToken, SymbolToken) and
+            prev is None
+        ):
+            return self.next_expression(tok)
+        elif typ == OperatorToken:
+            nxt = self.next_expression(None)
+            return self.next_expression(("operation", tok.value, prev, nxt))
+        elif typ == StartParamListToken:
+            args = self.multiple_expressions(
+                ListSeparatorToken, EndParamListToken)
+            return self.next_expression(("call", prev, args))
+        elif typ == StartFunctionDefToken:
+            params = self.parameters_list()
+            body = self.multiple_expressions(
+                StatementSeparatorToken, EndFunctionDefToken)
+            return self.next_expression(("function", params, body))
+        elif typ == AssignmentToken:
+            if type(prev) != SymbolToken:
+                raise Exception(
+                    "You can't assign to anything except a symbol.")
+            nxt = self.next_expression(None)
+            return self.next_expression(("assignment", prev, nxt))
+        else:
+            raise Exception("Unexpected token: " + str(tok))
+
+    def parameters_list(self):
+        if type(self.tokens.next) != ParamListPreludeToken:
+            return []  # If there's no colon, this function takes no args
+        self.tokens.move_next()
+        typ = type(self.tokens.next)
+        if typ != StartParamListToken:
+            raise Exception("':' must be followed by '(' in a function.")
+        self.tokens.move_next()
+        ret = self.multiple_expressions(ListSeparatorToken, EndParamListToken)
+        for param in ret:
+            if type(param) != SymbolToken:
+                raise Exception(
+                    "Only symbols are allowed in function parameter lists." +
+                    " I found: " + str(param) + "."
+                )
+        return ret
+
+    def multiple_expressions(self, sep, end):
+        ret = []
+        self.fail_if_at_end(end)
+        typ = type(self.tokens.next)
+        if typ == end:
+            self.tokens.move_next()
+        else:
+            arg_parser = Parser(self.tokens, (sep, end))
+            while typ != end:
+                p = arg_parser.next_expression(None)
+                if p is not None:
+                    ret.append(p)
+                typ = type(self.tokens.next)
+                self.tokens.move_next()
+                self.fail_if_at_end(end)
+        return ret
+
+    def fail_if_at_end(self, expected):
+        if self.tokens.next is None:
+            raise Exception("Hit end of file - expected '%s'." % expected)
+
+
+def parse(tokens_iterator):
+    parser = Parser(
+        PeekableStream(tokens_iterator),
+        (StatementSeparatorToken,)
+    )
+    while parser.tokens.next is not None:
+        p = parser.next_expression(None)
+        if p is not None:
+            yield p
+        parser.tokens.move_next()
---
+++ tests/test_parse2.py
@@ -0,0 +1,326 @@
+from graftlib.lex2 import (
+    lex,
+    AssignmentToken,
+    EndFunctionDefToken,
+    EndParamListToken,
+    ListSeparatorToken,
+    NumberToken,
+    OperatorToken,
+    ParamListPreludeToken,
+    StartFunctionDefToken,
+    StartParamListToken,
+    StatementSeparatorToken,
+    StringToken,
+    SymbolToken,
+)
+from graftlib.parse2 import parse
+
+# --- Utils ---
+
+
+def parsed(inp):
+    return list(parse(lex(inp)))
+
+
+# --- Parsing ---
+
+
+def test_Empty_file_produces_nothing():
+    assert parsed("") == []
+
+
+def test_Number_is_parsed_as_expression():
+    assert parsed("56;") == [NumberToken("56")]
+
+
+def test_Missing_semicolon_is_an_error():
+    try:
+        parsed("56")
+        fail("Should throw")
+    except Exception as e:
+        assert str(e) == "Hit end of file - expected ';'."
+
+
+def test_Sum_of_numbers_is_parsed_as_expression():
+    assert (
+        parsed("32 + 44;") ==
+        [
+            ("operation", "+", NumberToken("32"), NumberToken("44"))
+        ]
+    )
+
+
+def test_Difference_of_symbol_and_number_is_parsed_as_expression():
+    assert (
+        parsed("foo - 44;") ==
+        [
+            ("operation", "-", SymbolToken("foo"), NumberToken("44"))
+        ]
+    )
+
+
+def test_Multiplication_of_symbols_is_parsed_as_expression():
+    assert (
+        parsed("foo * bar;") ==
+        [
+            ("operation", "*", SymbolToken("foo"), SymbolToken("bar"))
+        ]
+    )
+
+
+def test_Variable_assignment_gets_parsed():
+    assert (
+        parsed("x = 3;") ==
+        [
+            ("assignment", SymbolToken("x"), NumberToken("3"))
+        ]
+    )
+
+
+def test_Function_call_with_no_args_gets_parsed():
+    assert (
+        parsed("print();") ==
+        [
+            ("call", SymbolToken("print"), [])
+        ]
+    )
+
+
+def test_Function_call_with_various_args_gets_parsed():
+    assert (
+        parsed("print( 'a', 3, 4 / 12 );") ==
+        [
+            (
+                "call",
+                SymbolToken("print"),
+                [
+                    StringToken("a"),
+                    NumberToken("3"),
+                    ("operation", "/", NumberToken("4"), NumberToken("12"))
+                ]
+            )
+        ]
+    )
+
+
+def test_Multiple_function_calls_with_no_args_get_parsed():
+    assert (
+        parsed("print()();") ==
+        [
+            ("call", ("call", SymbolToken("print"), []), [])
+        ]
+    )
+
+
+def test_Multiple_function_calls_with_various_args_get_parsed():
+    assert (
+        parsed("print( 'a', 3, 4 / 12 )(512)();") ==
+        [
+            (
+                "call",
+                (
+                    "call",
+                    (
+                        "call",
+                        SymbolToken("print"),
+                        [
+                            StringToken("a"),
+                            NumberToken("3"),
+                            (
+                                "operation",
+                                "/",
+                                NumberToken("4"),
+                                NumberToken("12")
+                            )
+                        ]
+                    ),
+                    [
+                        NumberToken("512")
+                    ]
+                ),
+                []
+            )
+        ]
+    )
+
+
+def test_Assigning_to_a_number_is_an_error():
+    try:
+        parsed("3 = x;")
+        fail("Should throw")
+    except Exception as e:
+        assert (
+            str(e) ==
+            "You can't assign to anything except a symbol."
+        )
+
+
+def test_Assigning_to_an_expression_is_an_error():
+    try:
+        parsed("x(4) = 5;")
+        fail("Should throw")
+    except Exception as e:
+        assert (
+            str(e) ==
+            "You can't assign to anything except a symbol."
+        )
+
+
+def test_Empty_function_definition_gets_parsed():
+    assert (
+        parsed("{};") ==
+        [
+            ("function", [], [])
+        ]
+    )
+
+
+def test_Missing_param_definition_with_colon_is_an_error():
+    try:
+        parsed("{:print(x););")
+        fail("Should throw")
+    except Exception as e:
+        assert (
+            str(e) ==
+            "':' must be followed by '(' in a function."
+        )
+
+
+def test_Multiple_commands_parse_into_multiple_expressions():
+    program = """
+    x = 3;
+    func = {:(a) print(a);};
+    func(x);
+    """
+    assert (
+        parsed(program) ==
+        [
+            ("assignment", SymbolToken('x'), NumberToken('3')),
+            (
+                "assignment",
+                SymbolToken('func'),
+                (
+                    "function",
+                    [SymbolToken('a')],
+                    [
+                        ("call", SymbolToken('print'), [SymbolToken('a')])
+                    ]
+                )
+            ),
+            ("call", SymbolToken('func'), [SymbolToken('x')])
+        ]
+    )
+
+
+def test_Empty_function_definition_with_params_gets_parsed():
+    assert (
+        parsed("{:(aa, bb, cc, dd)};") ==
+        [
+            (
+                "function",
+                [
+                    SymbolToken("aa"),
+                    SymbolToken("bb"),
+                    SymbolToken("cc"),
+                    SymbolToken("dd")
+                ],
+                []
+            )
+        ]
+    )
+
+
+def test_Function_params_that_are_not_symbols_is_an_error():
+    try:
+        parsed("{:(aa + 3, d)};"),
+        fail("Should throw")
+    except Exception as e:
+        assert (
+            str(e) ==
+            "Only symbols are allowed in function parameter lists. " +
+            "I found: " +
+            "('operation', " +
+            "'+', " +
+            "SymbolToken(value='aa'), " +
+            "NumberToken(value='3'))."
+        )
+
+
+def test_Function_definition_containing_commands_gets_parsed():
+    assert (
+        parsed('{print(3-4); a = "x"; print(a);};') ==
+        [
+            (
+                "function",
+                [],
+                [
+                    (
+                        "call",
+                        SymbolToken("print"),
+                        [
+                            (
+                                "operation",
+                                '-',
+                                NumberToken('3'),
+                                NumberToken('4')
+                            )
+                        ]
+                    ),
+                    ("assignment", SymbolToken("a"), StringToken("x")),
+                    ("call", SymbolToken("print"), [SymbolToken("a")])
+                ]
+            )
+        ]
+    )
+
+
+def test_Function_definition_with_params_and_commands_gets_parsed():
+    assert (
+        parsed('{:(x,yy)print(3-4); a = "x"; print(a);};') ==
+        [
+            (
+                "function",
+                [
+                    SymbolToken("x"),
+                    SymbolToken("yy")
+                ],
+                [
+                    (
+                        "call",
+                        SymbolToken("print"),
+                        [
+                            (
+                                "operation",
+                                '-',
+                                NumberToken('3'),
+                                NumberToken('4')
+                            )
+                        ]
+                    ),
+                    ("assignment", SymbolToken("a"), StringToken("x")),
+                    ("call", SymbolToken("print"), [SymbolToken("a")])
+                ]
+            )
+        ]
+    )
+
+
+def test_A_complex_example_program_parses():
+    example = """
+        double =
+            {:(x)
+                2 * x;
+            };
+
+        num1 = 3;
+        num2 = double( num );
+
+        answer =
+            if( greater_than( num2, 5 ),
+                {"LARGE!"},
+                {"small."}
+            );
+
+        print( answer );
+    """
+    parsed(example)
